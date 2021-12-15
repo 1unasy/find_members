d5127492e2b662953157c19b0162b3ada08ad82d
stella <tldus8267@gmail.com>
2021-12-15 17:14:21 +0900

    친구 목록 members 페이지에 나열하기

--- members/models.py
+++ members/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class FindMembers(models.Model):
+    name = models.CharField(max_length=20)
+    age = models.CharField(max_length=3)
+    birth = models.TextField()
+    email = models.TextField()
+
+    def __str__(self):
+        return f'[{self.pk}]{self.name}'
+
+    def get_absolute_url(self):
+        return "/members/{}".format(self.pk)
--- members/views.py
+++ members/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import FindMembers
 
 # Create your views here.
+members = FindMembers.objects.all()
+
+def index(request):
+    return render(
+        request,
+        'members/index.html',
+        {
+            'members' : members
+        }
+    )
